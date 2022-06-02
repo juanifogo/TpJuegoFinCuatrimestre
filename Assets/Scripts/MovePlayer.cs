5d3552ef74f35fc92c23d0e5546af77def297b73
juanifogo <juanifogo@gmail.com>
2022-06-02 10:49:41 -0300

    Initial commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MovePlayer : MonoBehaviour
+{
+    public float moveSpeed, jumpForce;
+    public int maxJumps;
+    int hasJump;
+    Rigidbody rb;
+    void Start()
+    {
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-moveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(moveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space) && hasJump>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            hasJump = maxJumps;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TpJuegoFinCuatrimestre
+ 
